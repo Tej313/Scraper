78b515574e23102e13e01b09b29c83d716fe4408
Tej313 <153349011+Tej313@users.noreply.github.com>
2024-05-27 12:16:35 +0545

    Delete Scraper directory

--- Scraper/__init__.py
+++
@@ -1,2 +0,0 @@
-# Scraper/__init__.py
-# This file is required to make Python treat the directory as a package.
--- Scraper/helpers.py
+++
@@ -1,5 +0,0 @@
-# helpers.py
-# You can add helper functions here that might be used by beautiful_soup.py or other scripts
-
-def example_helper():
-    print("This is a helper function")
